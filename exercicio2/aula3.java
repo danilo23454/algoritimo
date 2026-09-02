14f331524e4a844823a15342ba7b5840e820bf83
danilo23454 <43889717+danilo23454@users.noreply.github.com>
2026-09-02 09:52:59 -0300

    Add files via upload

---
+++ exercicio2/aula3.java
@@ -0,0 +1,9 @@
+public class aula3 {
+    public static void main (String [] args ){ 
+        System.out.println("ola mundo.");
+        System.out.println("minha idade é: " +42+ "anos.");
+        System.out.println("o resultado é:" +(3+19 ==22) );
+
+         }
+
+         }
